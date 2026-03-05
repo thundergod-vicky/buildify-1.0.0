1b1229e3e1efdfd493e5aa2fd91a986738003a68
Souvik Basu <bsouvik986.com>
2026-03-05 17:32:03 +0530

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
             <FeaturesSection />
             <FaqSection />
             <OurTeamSection />
-            <PricingSection />
+            {/* <PricingSection /> */}
             <TestimonialSection />
         </main>
     );
