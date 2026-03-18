cb8e2dbc43d794fd56911c0cbb1057595349640f
Souvik Basu <bsouvik986.com>
2026-03-18 19:06:09 +0530

    config: Update Cross-Origin-Embedder-Policy to `credentialless` in Next.js configuration.

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Cross-Origin-Embedder-Policy",
-            value: "require-corp",
+            value: "credentialless",
           },
         ],
       },
